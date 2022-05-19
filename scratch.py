74e428544d01b9e2b3d60055e8bc64efe80f41b1
sahithi-thegreat <sahithididugu@gmail.com>
2022-05-19 18:26:11 -0400

    initial commit

---
+++ scratch.py
@@ -0,0 +1,6 @@
+# to check if each word in a string begins with a capital letter
+a = input('enter the string')
+if a.istitle():
+    print('each word in a string begins with a capital letter')
+else :
+    print('each word in a string doen\'t begins with a capital letter')
